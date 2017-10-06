df78d95b5e5c4977eccb78ff32398996ab6145f2
Chris Johnson <chris@johnsonch.com>
2017-10-05 19:26:13 -0500

    user tests from in class

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup 
+    @user = User.new
+    @user.email = "bart@simpsons.com"
+  end 
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "user should have an email" do
+    @user.email = nil
+    assert_not @user.valid?
+  end
+  
+  test "user email must be longer than 5 characters" do
+    assert @user.valid?
+  end
+  
+  test "an email less than 5 characters is not valid" do
+    @user.email = "test"
+    assert_not @user.valid?
+  end
+  
 end

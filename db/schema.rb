713baacb41788298bd64923f4530cb7c2bbd7187
Chris Johnson <chris@johnsonch.com>
2017-10-12 19:43:08 -0500

    added user creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005230252) do
+ActiveRecord::Schema.define(version: 20171013003222) do
 
   create_table "turns", force: :cascade do |t|
     t.string "noun"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171005230252) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

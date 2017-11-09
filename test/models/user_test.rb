6e3b8f7dae0f79e57ce4b1b91a943c622d71e8a5
Chris Johnson <chris@johnsonch.com>
2017-11-09 17:38:39 -0600

    fixed user test by adding password info

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,28 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
-  def setup 
+
+  def setup
     @user = User.new
     @user.email = "bart@simpsons.com"
-  end 
-  
+    @user.password = 'foobar'
+    @user.password_confirmation = 'foobar'
+  end
+
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "user should have an email" do
     @user.email = nil
     assert_not @user.valid?
   end
-  
+
   test "user email must be longer than 5 characters" do
     assert @user.valid?
   end
-  
+
   test "an email less than 5 characters is not valid" do
     @user.email = "test"
     assert_not @user.valid?
   end
-  
+
 end
